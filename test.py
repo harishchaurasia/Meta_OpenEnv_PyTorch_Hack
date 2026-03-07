d3c714d92b9ce96c4bb05f7ab267c28a2f1ff0d7
Harish <harishchaurasia143@gmail.com>
2026-03-07 15:47:24 -0800

    added dockerfile and dockerignore

---
+++ test.py
@@ -0,0 +1,32 @@
+# from adaptive_nav.openenv_client import NavEnvClient
+# from adaptive_nav.models import NavAction
+
+# client = NavEnvClient("ws://localhost:8000")
+
+# obs = client.reset()
+
+# print(obs)
+
+# from adaptive_nav.models import NavAction
+
+# result = client.step(NavAction(action_id=3))
+# print(result)
+# print(result.observation, result.reward)
+
+from adaptive_nav.openenv_client import NavEnvClient
+from adaptive_nav.models import NavAction
+import random
+
+ACTION_NAMES = ["up", "down", "left", "right", "interact", "wait"]
+
+client = NavEnvClient("ws://localhost:8000")
+
+result = client.reset()
+print("reset ok")
+
+for i in range(10):
+    action_id = random.randint(0, 5)
+    result = client.step(NavAction(action_id=action_id))
+    print(i, ACTION_NAMES[action_id], result.reward, result.done)
+    if result.done:
+        break
